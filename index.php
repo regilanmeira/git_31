d15b952f4050f067e41f3e56c9e403502a3158a7
regilanmeira <regilan@hotmail.com>
2024-09-11 09:31:15 -0300

    Criado o estilo CSS

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="estilo.css" >
 </head>
 <body>
-    <h1>Versinamento com GIT/GITHUB</h1>
+    <h1>Versionamento com GIT/GITHUB</h1>
     <h2>Aula 11/09</h2>
     <p>Executando git commit</p>
 </body>
